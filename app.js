f27b1889470fb2224db0ab168fc457525c0320a0
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-04-25 10:53:23 -0500

    mongodb conn

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+//Conexion a la base de datos
+mongoose.connection.openUri(
+  "mongodb://localhost:27017/hospitalDB",
+  (err, res) => {
+    if (err) throw err;
+    console.log("Base de datos:online");
+  }
+);
 
 //escuchar express
 app.listen(3000, () => {
