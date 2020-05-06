1dd316ec97ace5e1983eb94595f1d1a2c89bdcc8
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-05 21:24:10 -0500

    busqueda de colecciones especificas

--- src/routes/busqueda.js
+++ src/routes/busqueda.js
@@ -1,10 +1,50 @@
 const express = require("express");
 const app = express();
 
+// rutas
 var Hospital = require("../models/hospital");
 var Medico = require("../models/medico");
 var Usuario = require("../models/usuario");
-// rutas
+//  =============================
+// Busqueda por coleccion
+// =============================
+app.get("/coleccion/:tabla/:busqueda", (req, res) => {
+  var busqueda = req.params.busqueda;
+  var tabla = req.params.tabla;
+  var regex = new RegExp(busqueda, "i");
+
+  var promesa;
+
+  switch (tabla) {
+    case "usuarios":
+      promesa = buscarUsuarios(busqueda, regex);
+      break;
+    case "medicos":
+      promesa = buscarMedicos(busqueda, regex);
+      break;
+    case "hospitales":
+      promesa = buscarHospitales(busqueda, regex);
+      break;
+    default:
+      return res.status(400).json({
+        ok: false,
+        mensaje:
+          "Los tipos de búsqueda solo son: usuarios, medicos y hospitales",
+        error: { message: "Tipo de tabla/coleccion no valida" },
+      });
+  }
+
+  promesa.then((data) => {
+    res.status(200).json({
+      ok: true,
+      [tabla]: data,
+    });
+  });
+});
+
+// =============================
+// Busqueda general
+// =============================
 app.get("/todo/:busqueda", (req, res, next) => {
   var busqueda = req.params.busqueda;
   var regex = new RegExp(busqueda, "i");
