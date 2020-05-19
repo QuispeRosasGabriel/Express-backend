1b54ef84c226c50b3ca23fe7c7c4c1eb31517263
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-18 21:26:13 -0500

    corrigiendo respuesta de upload route

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -133,7 +133,7 @@ function subirPorTipo(tipo, id, nombreArchivo, res) {
         return res.status(200).json({
           ok: true,
           mensaje: "Imagen de médico actualizada",
-          usuario: medicoActualizado,
+          medico: medicoActualizado,
         });
       });
     });
@@ -162,7 +162,7 @@ function subirPorTipo(tipo, id, nombreArchivo, res) {
         return res.status(200).json({
           ok: true,
           mensaje: "Imagen de hospital actualizada",
-          usuario: hospitalActualizado,
+          hospital: hospitalActualizado,
         });
       });
     });
